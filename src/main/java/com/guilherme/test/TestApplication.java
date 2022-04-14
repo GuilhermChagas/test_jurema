5b3cb89c71ff4818ffdd29c4263f867033709bb7
Guilherme Chagas <guilhermehocc@outlook.com>
2022-04-13 23:48:54 -0300

    add dependencies

--- src/main/java/com/guilherme/test/TestApplication.java
+++ src/main/java/com/guilherme/test/TestApplication.java
@@ -1,13 +1,26 @@
 package com.guilherme.test;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+//		SpringApplication.run(TestApplication.class, args);
+
+		
+		for (int i = 0; i <= 100; i++) {
+			System.out.println((i%3==0 && i%5==0 ? 
+					i + "FizzBuzz" : i%3==0 ? 
+							i + "Fizz" : i%5==0 ? 
+									i + "Buzz" : i));
+			
+//			if(i%3==0 && i%5==0) System.out.println(i + "FizzBuzz");
+//			else if(i%3==0) System.out.println(i + "Fizz");
+//			else if(i%5==0) System.out.println(i + "Buzz");
+//			else System.out.println(i);
+		}
 	}
 
+	
 }
