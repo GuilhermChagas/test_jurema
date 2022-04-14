c29a39028730d827b1621c7c118e5469897399ca
Guilherme Chagas <40358638+GuilhermChagas@users.noreply.github.com>
2022-04-13 22:02:58 -0300

    Start

---
+++ src/test/java/com/guilherme/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
